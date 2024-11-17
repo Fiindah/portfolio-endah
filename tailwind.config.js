e2209a56ce91d41d8ad16f57aced15a4ffa4ac75
Endah F N <76398931+Fiindah@users.noreply.github.com>
2024-11-17 22:24:19 +0700

    Add files via upload
    update dan perbaikan

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#4f46e5',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
+
